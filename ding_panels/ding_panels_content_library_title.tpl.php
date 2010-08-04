445a91383dea6898a6377802fcc539a86f4c0a3d
Mikkel Hoegh <mikkel@hoegh.org>
2010-08-04 21:39:53 +0200

    Making library title pane more configurable.
    * Settings for navigation links and imagecache preset added.
    * Moved out of library.inc into its own file.
    * Updated Panels pages and handlers to reflect these changes.

--- ding_panels/ding_panels_content_library_title.tpl.php
+++ ding_panels/ding_panels_content_library_title.tpl.php
@@ -1,12 +1,20 @@
-<?php if($display_header_image){  ?>
+<?php
+// $Id$
+
+/**
+ * @file
+ * Render the library title Panels content type.
+ */
+?>
+
+<?php if ($header_image): ?>
 <div class="picture">
   <div class="picture-inner">
-  <?php if($node->field_image['0']['filepath']){ ?>
-    <?php print theme('imagecache', '680_200_crop', $node->field_image['0']['filepath']); ?>      
-  <?php } ?>
+  <?php print $header_image; ?>
   </div>
 </div>
-<?php } ?>
+<?php endif; ?>
 
 <h1><?php print $library_title; ?></h1>
-<?php  print $library_navigation; ?>
+<?php print $library_navigation; ?>
+
