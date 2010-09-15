2e1d1477cee37c5707e32252d604828f03269d0d
Hasse R. Hansen <hasse@ramlev.dk>
2010-09-15 08:37:23 +0200

    Moved ding reservations javascript button behaviours to an external js file

---
+++ ding_reservation/ding_reservation.buttons.js
@@ -0,0 +1,15 @@
+// $Id$
+
+/**
+ * Javascript helpers for Ding reservation interaction buttons.
+ */
+Drupal.behaviors.dingReservationButtons = function () {
+  Drupal.tingButtons.dialogButton(".reserve-now", { 
+    buttons: function(buttons, event, data) {
+      if (data.status === "success" && $(event.target).hasClass("reserve-now")) {
+        $count = $("#account-profile .reservations strong");
+        $count.text(parseInt($count.text(), 10) + 1);
+      }
+    }
+  });
+}
