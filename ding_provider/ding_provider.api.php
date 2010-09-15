00c23e807ebe2431df316b43c6ae0ab4066afdca
Thomas Fini Hansen <xen@xen.dk>
2010-09-15 13:57:43 +0200

    New ding_provider module for backend abstraction.

---
+++ ding_provider/ding_provider.api.php
@@ -0,0 +1,28 @@
+<?php
+
+// $Id$
+
+/**
+ * @file
+ * Documentation for ding_provider hooks and pseudo-hooks.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Implemented by provider modules to announce their services.
+ */
+function hook_ding_provider_info() {
+  return array(
+    'provider_module' => array(
+      'name' => 'Administrative title',
+    ),
+  );
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
