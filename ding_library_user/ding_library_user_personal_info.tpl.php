2357e7e0aa9eff1abf24bf7c6a51512d73105bb5
Thomas Fini Hansen <xen@xen.dk>
2010-10-08 13:23:56 +0200

    Killed ding_library_user_personal_info content_type and moved its content to standard user profile. Updated user view to use it. Added ding_library_user_required function. ding_library_user_personal_info.tpl.php now uses display_name and email instead of $library_user_data. Reservation using menu loader.

--- ding_library_user/ding_library_user_personal_info.tpl.php
+++ ding_library_user/ding_library_user_personal_info.tpl.php
@@ -10,13 +10,13 @@
   <h4><?php print t('Basic information'); ?></h4>
   <dl>
     <dt><?php print t('Name'); ?></dt>
-    <dd class="fn"><?php print check_plain($library_user_data['user_name']); ?></dd>
+    <dd class="fn"><?php print check_plain($account->display_name); ?></dd>
   </dl>
   <h4><?php print t('Contact information'); ?></h4>
   <dl>
     <dt><?php print t('E-mail address'); ?></dt>
-    <dd class="email"><?php print check_plain($library_user_data['mails'][0]['mail']); ?></dd>
+    <dd class="email"><?php print check_plain($account->mail); ?></dd>
     <dd><?php print $links; ?></dd>
   </dl>
-</div>    
+</div>
 
