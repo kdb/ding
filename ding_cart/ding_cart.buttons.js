5172a0a38a0dfebb9e76d75215dd69edef78d969
Thomas Fini Hansen <xen@xen.dk>
2010-11-17 13:33:03 +0100

    Moved cart support from providers to ding_reservation.

--- ding_cart/ding_cart.buttons.js
+++ ding_cart/ding_cart.buttons.js
@@ -4,13 +4,13 @@
  * Javascript helpers for Ding cart interaction buttons.
  */
 Drupal.behaviors.dingCartButtons = function () {
-  Drupal.tingButtons.dialogButton(".add-to-cart", { 
+  Drupal.tingButtons.dialogButton(".add-to-cart a", {
     buttons: function(buttons, event, data) {
-      if (data.status === 'success' && $(event.target).hasClass('add-to-cart')) {
+      if (data.status === 'success') {
         buttons[Drupal.t('View cart…')] = function () {
           window.location = data.cart_link;
         };
-        
+
         $count = $('#account-profile .cart .count');
         $count.text(parseInt($count.text(), 10) + 1);
       }
