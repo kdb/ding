8823dde6ac9a12e1a0af530ea29516a1c6e3d3ac
Mikkel Hoegh <mikkel@hoegh.org>
2011-03-12 16:25:22 +0100

    Refactored ding_library_map.
    * Switched to OpenLayers for more flexibility.
    * Rewrote map JavaScript.
    * Converted module to a Feature.
    * New markers.

--- ding_library/ding_library_map/js/ding_library_map.js
+++ ding_library/ding_library_map/js/ding_library_map.js
@@ -1,59 +1,155 @@
 /**
  * @file
- * JavaScript object to handle our library map.
+ * JavaScript to handle our library map.
  */
 
-Drupal.dingLibraryMap = function(mapId, options) {
-  this.mapId = mapId;
+/**
+ * Prototype for controlling the map.
+ */
+Drupal.DingLibraryMapController = function (mapId, options) {
+  var self = this;
+  self.markers = {};
+  self.mapId = mapId;
+  self.options = options;
 
-  this.getMap = function() {
-    return Drupal.gmap.getMap(this.mapId);
-  };
+  /**
+   * Set up the map with markers, etc.
+   */
+  self.init = function () {
+    var iconOffset, iconSize, initLat, initLon;
+    // Init should only be run once.
+    if (self.isInitialised) { return; }
+    self.isInitialised = true;
+    self.mapContainer = $("#" + mapId);
 
-  this.info = function(mapId, options) {
-    this.fullDayNames = options.fullDayNames;
-    this.shortDayNames = options.shortDayNames;
+    // Get the map object instance from jQuery data.
+    self.map = self.mapContainer.data('openlayers');
 
-    this.init = function() {
-      $('.gmap-'+this.mapId+'-gmap').after(
-      '<div id="library-info" style="display: none;">'+
-        '<div class="frame">'+
-          '<div class="inner">'+
-            '<h3 class="name"></h3>'+
-            '<div class="address">'+
-              '<div class="street"></div>'+
-              '<div><span class="postal-code"></span> <span class="city"></span></div>'+
-            '</div>'+
-            '<dl class="opening-hours">'+
-            '</dl>'+
-          '</div>'+
-        '</div>'+
-      '</div>');
+    // Determine the icon's offset, so the tip of the arrow on the icon
+    // will be at the marker's location.
+    iconOffset = new OpenLayers.Pixel(-7, -28);
 
-      var info = this;
-      this.getMap().bind('mouseovermarker', function(object) {
-        info.showInfo(object);
-      });
-
-      $('#library-info').bind('mouseleave', function() {
-        info.hideInfo();
-      });
-
-      // Triggers for hiding info: mouseout, resize, zoom, move.
-      this.getMap().bind('widthchange heightchange zoom move', function() {
-        info.hideInfo();
-      });
+    // Set up the icons we use for markers.
+    iconSize = new OpenLayers.Size(22, 30);
+    self.icons = {
+      'closed': new OpenLayers.Icon(self.options.path + '/markers/marker_closed.png', iconSize, iconOffset),
+      'open': new OpenLayers.Icon(self.options.path + '/markers/marker_open.png', iconSize, iconOffset)
     };
 
-    this.showInfo = function(object) {
-      var day, days, section, sectionDays, nextDay, startTime, endTime, startDay, endDay;
-      // Add address attributes
+    // Draw all the markers on the map.
+    self.markersLayer = new OpenLayers.Layer.Markers("Markers");
+    self.map.openlayers.addLayer(self.markersLayer);
+    $.each(self.options.markers, function (key, markerData) {
+      self.markers[key] = self.addMarker(markerData);
+    });
+
+    // Add expand/contract control to the map.
+    $('<a class="resize expand" href="#">Expand map</a>')
+      .appendTo(self.mapContainer)
+      .toggle(function(event) {
+        self.resizeMap(450);
+        $(event.target).toggleClass('expand').toggleClass('contract');
+      }, function(event) {
+        self.resizeMap(200);
+        $(event.target).toggleClass('expand').toggleClass('contract');
+      });
+
+    // If a position is specified in the URL params, go there.
+    initLat = $.url.param('lat');
+    initLon = $.url.param('lon');
+    if (initLat && initLon) {
+      self.map.openlayers.setCenter(self.convertPosition(initLon, initLat));
+      self.map.openlayers.zoomTo(14);
+      $.scrollTo(self.mapContainer, '500', { offset: -20 });
+    }
+
+    // Set up the info bubble that appears when hovering a marker.
+    self.infoBox = $('<div id="library-info" style="display: none;">' +
+        '<div class="frame">' +
+          '<div class="inner">' +
+            '<h3 class="name"></h3>' +
+            '<div class="address">' +
+              '<div class="street"></div>' +
+              '<div><span class="postal-code"></span> <span class="city"></span></div>' +
+            '</div>' +
+            '<dl class="opening-hours"></dl>' +
+          '</div>' +
+        '</div>' +
+      '</div>');
+
+    self.infoBox.appendTo(self.mapContainer);
+
+    // Hide the infobox when the user leaves it. Since it covers the
+    // marker, that is a practical way to get rid of it :)
+    self.infoBox.bind('mouseleave', function () {
+      self.infoBox.hide();
+    });
+
+    // Triggers for hiding info: mouseout, resize, zoom, move.
+    self.mapContainer.bind('widthchange heightchange zoom move', self.infoBox.hide());
+  };
+
+  /**
+   * Add a marker to the map.
+   */
+  self.addMarker = function (markerData) {
+    var position = new OpenLayers.LonLat(markerData.longitude, markerData.latitude).transform(new OpenLayers.Projection('EPSG:4326'), self.map.openlayers.projection),
+        marker = new OpenLayers.Marker(position, self.icons.closed.clone());
+
+    self.markersLayer.addMarker(marker);
+
+    marker.events.register('mouseover', marker, function () {
+      self.updateInfoBox(marker, markerData);
+    });
+
+    return marker;
+  };
+
+  /**
+   * Resize the map to a new size.
+   *
+   * Currently, only the height can be changed.
+   */
+  self.resizeMap = function (height) {
+    var center = self.map.openlayers.getCenter();
+
+    self.infoBox.hide();
+
+    // The height is some times set on both the map container and the
+    // parent. This causes issues with resing, so remove it.
+    self.mapContainer.parent().height('auto');
+
+    self.mapContainer.animate({'height': height + 'px'}, 1000, 'swing', function () {
+      self.map.openlayers.updateSize();
+      self.map.openlayers.panTo(center);
+    });
+
+  };
+
+  /**
+   * Generates a LonLat object from lon and lat parameters.
+   *
+   * These are transformed to our current map projection.
+   */
+  self.convertPosition = function (lon, lat) {
+    var position = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection('EPSG:4326'), self.map.openlayers.projection);
+
+    return position;
+  };
+
+  /**
+   * Update the infobox for the marker being hovered on.
+   */
+  self.updateInfoBox = function(marker, markerData) {
+      var day, days, section, sectionDays, nextDay, startTime, endTime, startDay, endDay, point;
+      // Add address attributes. Each field has a container with the
+      // corresponding class name.
       $.each(['name', 'street', 'postal-code', 'city'], function (i, val) {
-        $('#library-info .' + val).text(object[val]);
+        self.infoBox.find('.' + val).text(markerData[val]);
       });
 
       //Add opening hours
-      section = $('#library-info .opening-hours').empty();
+      section = self.infoBox.find('.opening-hours').empty();
       days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']; //collection of weekdays to use for iteration
       day = 0; //current day index
        //iterate until end of week
@@ -61,27 +157,27 @@ Drupal.dingLibraryMap = function(mapId, options) {
         sectionDays = [days[day]]; //add current day to to section
         nextDay = day + 1;
 
-        if (object.opening_hours[[days[day]]] &&
-            object.opening_hours[[days[day]]].length > 0) {
-          startTime = object.opening_hours[[days[day]]][0].open;
-          endTime = object.opening_hours[[days[day]]][0].close;
+        if (markerData.opening_hours[[days[day]]] &&
+            markerData.opening_hours[[days[day]]].length > 0) {
+          startTime = markerData.opening_hours[[days[day]]][0].open;
+          endTime = markerData.opening_hours[[days[day]]][0].close;
 
           while ( (nextDay < days.length) &&
-                  (object.opening_hours[[days[nextDay]]].length > 0) &&
+                  (markerData.opening_hours[[days[nextDay]]].length > 0) &&
                   (startTime !== null) && (endTime !== null) &&
-                  (startTime == object.opening_hours[[days[nextDay]]][0].open) &&
-                  (endTime == object.opening_hours[[days[nextDay]]][0].close)) {
+                  (startTime == markerData.opening_hours[[days[nextDay]]][0].open) &&
+                  (endTime == markerData.opening_hours[[days[nextDay]]][0].close)) {
             sectionDays.push(days[nextDay]); //if following days have same hours as current day then add these days to section
-            nextDay++;
+            nextDay += 1;
           }
 
           if (sectionDays.length > 1) {
-            startDay = this.shortDayNames[sectionDays.shift()];
-            endDay = this.shortDayNames[sectionDays.pop()];
+            startDay = self.options.shortDayNames[sectionDays.shift()];
+            endDay = self.options.shortDayNames[sectionDays.pop()];
             sectionDays = startDay.substr(0, 1).toUpperCase()+startDay.substr(1)+'-'+endDay.substr(0, 1).toUpperCase()+endDay.substr(1); //use short day names if section spans more than one day
           }
           else {
-            sectionDays = this.fullDayNames[sectionDays.shift()]; //use full day name for section spanning a single day
+            sectionDays = self.options.fullDayNames[sectionDays.shift()]; //use full day name for section spanning a single day
           }
 
           startTime = (startTime !== null) ? startTime.substr(0, startTime.lastIndexOf(':')) : '';
@@ -97,89 +193,46 @@ Drupal.dingLibraryMap = function(mapId, options) {
         day = nextDay; //step to day after last day in current section
       }
 
-      //Update css classes to reflect open state
-      $('#library-info').removeClass('open').removeClass('closed').addClass(object.state);
-
-      //Add click handler
-      $('#library-info').unbind('click').click(function() {
-        window.location = object.url;
+      // Add click handler
+      self.infoBox.unbind('click').click(function() {
+        window.location = markerData.url;
       });
 
       //Position and show info
-      point = this.getMap().map.fromLatLngToContainerPixel(object.marker.getLatLng());
-      $('#library-info').css({ 'left': (point.x-5)+'px', 'top': (point.y- $('#library-info').outerHeight())+'px' }).show();
-    };
-
-    this.hideInfo = function() {
-      $('#library-info').hide();
-    };
-
-    this.init();
+      point = self.map.openlayers.getPixelFromLonLat(marker.lonlat);
+      self.infoBox.css({ 'left': (point.x - 7) + 'px', 'top': (point.y - self.infoBox.outerHeight()) + 'px' }).show();
   };
 
-  this.resize = function(mapId) {
-
-    this.init = function() {
-      $('.gmap-'+this.mapId+'-gmap').after(
-        '<a class="resize expand" href="#">'+
-          'Expand map'+
-        '</a>');
-
-      var resize = this;
-      $('a.resize').toggle(function(event) {
-        resize.resizeMap(450);
-        $(event.target).toggleClass('expand').toggleClass('contract');
-      }, function(event) {
-        resize.resizeMap(200);
-        $(event.target).toggleClass('expand').toggleClass('contract');
-      });
-    };
-
-    this.resizeMap = function(size) {
-      var resize, center;
-      this.hideInfo();
-      resize = this;
-      center = this.getMap().map.getCenter();
-      $('.gmap-'+this.mapId+'-gmap').animate({ 'height' : size+'px' }, 1000, 'swing', function() {
-        resize.getMap().map.checkResize();
-        resize.getMap().map.panTo(center);
-      });
-    };
-
-    this.init();
-  };
-
-  this.goTo = function(mapId, options) {
-
-    this.init = function() {
-      var map, lat, long;
-      map = this.getMap();
-
-      lat = $.url.param('lat');
-      long = $.url.param('long');
-      if (lat && long) {
-        map.bind('ready', function() {
-          map.map.setCenter(new GLatLng(lat, long), 14);
-          $.scrollTo(map.map.getContainer(), '500', { offset: -20 });
-        });
-      }
-
-      $('.link-card a').click(function() {
-        geo = $(this).parents('.content:first').find('.geo');
-        map.map.setCenter(new GLatLng(geo.find('.latitude').text(), geo.find('.longitude').text()), 14);
-        $.scrollTo(map.map.getContainer(), '500', { offset: -20 });
-        return false;
-      });
-    };
-
-    this.init();
-  };
-
-  // Initialize the map if Drupal.gmap is available.
-  if (Drupal.gmap) {
-    this.info(mapId, options);
-    this.resize(mapId, options);
-    this.goTo(mapId, options);
+  // Initialize the map if OpenLayers is available.
+  if (OpenLayers) {
+    self.init();
   }
+
+  return self;
 };
+
+// Set up our controller when the document is loaded.
+jQuery(function($) {
+  var lmc = new Drupal.DingLibraryMapController('library-map', Drupal.settings.dingLibraryMap);
+
+  // On the page with the library list, there are links to view the
+  // library on the map on each list item. We want to catch those clicks
+  // and move the map to the area in question.
+  $('#content-main .view-on-map-link').click(function () {
+    var link = $(this),
+        href = link.attr('href'),
+        latMatch = /lat=(\d+\.?\d*)/.exec(href),
+        lonMatch = /lon=(\d+\.?\d*)/.exec(href);
+
+    // If we have coordinates for both dimensions, center the map and
+    // scroll the browser to it.
+    if (latMatch[1] && lonMatch[1]) {
+      lmc.map.openlayers.setCenter(lmc.convertPosition(lonMatch[1], latMatch[1]));
+      lmc.map.openlayers.zoomTo(14);
+      $.scrollTo(lmc.mapContainer, '500', { offset: -20 });
+    }
+
+    return false;
+  });
+});
 
