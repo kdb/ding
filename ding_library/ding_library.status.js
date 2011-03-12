7319cf5081e19dc0c1cc5a83ccc7f49ef8ddbdfc
Mikkel Hoegh <mikkel@hoegh.org>
2011-03-12 20:10:50 +0100

    Refactored status checker.
    No difference in functionality, but nicer JavaScript.

--- ding_library/ding_library.status.js
+++ ding_library/ding_library.status.js
@@ -1,29 +1,66 @@
-// $Id$
-
 /**
  * @file
  * JavaScript behavior to update library open/closed status dynamically.
  */
 
-Drupal.behaviors.dingLibraryStatus = function () {
-  var settings = Drupal.settings.officeHours['node' + Drupal.settings.dingLibraryNids[0]];
-  $.getJSON(settings.callback + '/' + Drupal.settings.dingLibraryNids.join(',') + '/' + settings.field_name, {}, function (response, textStatus) {
-    $.each(response.data, function (nid, hoursData) {
-      $('#node-' + nid + ' .library-openstatus')
-        // Update the label.
-        .text(hoursData.status_local)
-        // Remove the existing status classes.
-        .removeClass('open')
-        .removeClass('closed')
-        // Add the current status as a class.
-        .addClass(hoursData.status);
+
+/**
+ * Prototype for updateting library opening status.
+ */
+Drupal.DingLibraryStatusUpdater = function () {
+  var self = this;
+
+  /**
+   * Constructor for the updater.
+   */
+  self.init = function () {
+    if (self.isInitialised) { return; }
+    self.isInitialised = true;
+
+    // Get the settings from the Drupal settings object.
+    self.settings = Drupal.settings.officeHours['node' + Drupal.settings.dingLibraryNids[0]];
+
+    // When done initialising, call refresh the first time.
+    self.refresh();
+  };
+
+  /**
+   * Refresh the opening status.
+   */
+  self.refresh = function () {
+    $.getJSON(self.settings.callback + '/' + Drupal.settings.dingLibraryNids.join(',') + '/' + self.settings.field_name, {}, function (response, textStatus) {
+      $.each(response.data, function (nid, hoursData) {
+        $('#node-' + nid + ' .library-openstatus')
+          // Update the label.
+          .text(hoursData.status_local)
+          // Remove the existing status classes.
+          .removeClass('open')
+          .removeClass('closed')
+          // Add the current status as a class.
+          .addClass(hoursData.status);
+      });
     });
-  });
+  };
+
+  /**
+   * Helper function to refresh status regularly.
+   */
+  self.refreshEvery = function (interval) {
+    window.clearInterval(self.refreshInterval);
+
+    self.refreshInterval = window.setInterval(self.refresh, interval);
+  };
+
+  self.init();
+  return self;
 };
 
-// Object to hold our globally accessible stuff.
-Drupal.dingLibraryStatus = {
+// Set up our status update when the document is loaded.
+jQuery(function($) {
+  // Set up status updater.
+  var updater = new Drupal.DingLibraryStatusUpdater();
+
   // Refresh library status every 5 minutes.
-  'interval': setTimeout(Drupal.behaviors.dingLibraryStatus, 300000)
-}
+  updater.refreshEvery(300000);
+});
 
