22cd8c22f4314c911010e1325528e08b9cdb534b
Mikkel Hoegh <mikkel@hoegh.org>
2011-05-19 14:56:50 +0200

    JSLinted.

--- ding_reservation/ding_reservation.buttons.js
+++ ding_reservation/ding_reservation.buttons.js
@@ -1,4 +1,3 @@
-
 /**
  * Javascript helpers for Ding reservation interaction buttons.
  */
@@ -6,9 +5,10 @@ Drupal.behaviors.dingReservationButtons = function () {
   Drupal.tingButtons.dialogButton(".reserve-now", { 
     buttons: function(buttons, event, data) {
       if (data.status === "success" && $(event.target).hasClass("reserve-now")) {
-        $count = $("#account-profile .reservations strong");
+        var $count = $("#account-profile .reservations strong");
         $count.text(parseInt($count.text(), 10) + 1);
       }
     }
   });
-}
+};
+
